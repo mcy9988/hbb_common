c591627f453070454a381b96c42d3e294fc0b1b3
mcy9988 <62431398+mcy9988@users.noreply.github.com>
2025-07-19 12:07:16 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["mcy9988.mycloudnas.com"];
+pub const RS_PUB_KEY: &str = "9C+SM3m9bMtpASceTcP6RAaxrJjwhO8PLMgZqGiK+KE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
